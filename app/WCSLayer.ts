03e9b07e12d37ceb8c77959d298fc539d3fa2e8c
Dzenan Dumpor <dumpor@outlook.com>
2018-11-08 22:37:35 +0100

    non working version

--- app/WCSLayer.ts
+++ app/WCSLayer.ts
@@ -1,10 +1,13 @@
 // To make TypeScript stop infering and
 // complaining about undeclared variable.
 // Imported globally with a normal script tag
-declare var Tiff: any;
+declare var TIFFParser: any;
+
+import Extent = require("esri/geometry/Extent");
+import BaseElevationLayer = require("esri/layers/BaseElevationLayer");
 
 const getTile = (url: string) => {
-  const promise = new Promise((resolve, reject) => {
+  const promise = new Promise<string>((resolve, reject) => {
     var xhrRequest = new XMLHttpRequest();
 
     xhrRequest.open("GET", url, true);
@@ -15,10 +18,15 @@ const getTile = (url: string) => {
         // request is done
 
         if (xhrRequest.status === 200) {
-          resolve(xhrRequest.response);
-        }
+          const tiffParser = new TIFFParser();
 
-        reject(xhrRequest.responseText);
+          tiffParser.parseTIFF(xhrRequest.response);
+          const dataUrl = tiffParser.canvas.toDataURL();
+
+          resolve(dataUrl);
+        } else {
+          reject("Failed to get response from WCS");
+        }
       }
     };
 
@@ -28,16 +36,91 @@ const getTile = (url: string) => {
   return promise;
 };
 
-class WCSLayer {
-  private tileSpanX: number;
-  private tileSpanY: number;
+const WCSElevationLayer = BaseElevationLayer.createSubclass({
+  // This function must associate elevation tiles with temperature
+  // values. This is achieved by getting the bounds (or extent)
+  // of the tile and fetching a temperature image based on
+  // the returned extent
 
-  private wcsWidth: number;
-  private wcsHeight: number;
+  load: function() {
+    debugger;
+    return this;
+  },
 
-  constructor() {
-    // TODO accept url
+  fetchTile: function(level: number, row: number, col: number) {
+    debugger;
+    var bounds = this.getTileBounds(level, row, col);
+    var tileSize = this.tileInfo.size[0] + 1;
+    var extent = new Extent({
+      xmin: bounds[0],
+      ymin: bounds[1],
+      xmax: bounds[2],
+      ymax: bounds[3],
+      spatialReference: this.spatialReference
+    });
+
+    const { xmin, ymin, xmax, ymax } = extent;
+
+    const format = "xyz";
+    const url = `https://wms.geonorge.no/skwms1/wcs.dtm?SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&FORMAT=${format}&WIDTH=${tileSize}&HEIGHT=${tileSize}&COVERAGE=land_utm33_10m&crs=EPSG:25833&BBOX=${xmin},${ymin},${xmax},${ymax}`;
+
+    const noDataValue = 0;
+
+    return getTile(url)
+      .then(response => {
+        const xyzArray = response.split("\n");
+
+        return {
+          values: new Array(256 * 256),
+          width: tileSize, // pixelBlock.width,
+          height: tileSize, //pixelBlock.height,
+          noDataValue: noDataValue
+        };
+      })
+      .catch(error => {
+        console.warn("WCS failed me :(");
+        return {
+          values: new Array(256 * 256),
+          width: tileSize, // pixelBlock.width,
+          height: tileSize, //pixelBlock.height,
+          noDataValue: noDataValue
+        };
+      });
+
+    // fetch the image representing temperature for the extent of the tile.
+    // this method returns the pixel data of the image for the extent
+    // of the given elevation tile
+    // return this._temperature.fetchImage(extent, tileSize, tileSize).then(
+    //   function(data: any) {
+    //     var pixelBlock = data.pixelData.pixelBlock;
+    //     // contains the temperature values of each pixel in the image
+    //     var elevations = pixelBlock.pixels[0];
+    //     var stats = pixelBlock.statistics[0];
+    //     // pixels that don't contain any temperature values
+    //     var noDataValue = stats.noDataValue;
+
+    //     elevations.forEach(function(value: any, index: any, pixelData: any) {
+    //       if (value !== noDataValue) {
+    //         // multiply temperatures by the given factor
+    //         pixelData[index] = value * factor;
+    //       } else {
+    //         // areas with no temperature data (land)
+    //         // will be assigned the average sea surface
+    //         // temperature (17 degrees Celsius)
+    //         pixelData[index] = 17 * factor;
+    //       }
+    //     });
+
+    //     // return the modified temperatures as elevations
+    //     return {
+    //       values: elevations,
+    //       width: pixelBlock.width,
+    //       height: pixelBlock.height,
+    //       noDataValue: noDataValue
+    //     };
+    //   }.bind(this)
+    // );
   }
-}
+});
 
-export default WCSLayer;
+export = WCSElevationLayer;
--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,7 @@ import EsriMap = require("esri/Map");
 import Basemap = require("esri/Basemap");
 import WMTSLayer = require("esri/layers/WMTSLayer");
 import SceneView = require("esri/views/SceneView");
+import watchUtils = require("esri/core/watchUtils");
 
 import WCSLayer = require("./WCSLayer");
 
@@ -11,6 +12,15 @@ const topoGraatoneWmts = new WMTSLayer({
   copyright: "Kartverket",
   activeLayer: {
     id: "topo4graatone"
+  },
+  fullExtent: {
+    xmin: -2500000,
+    ymin: 3499999.999999999,
+    xmax: 3045984.000000001,
+    ymax: 9045984,
+    spatialReference: {
+      wkid: 25833
+    }
   }
 });
 
@@ -20,13 +30,24 @@ const basemap = new Basemap({
   id: "topo4graatone"
 });
 
+debugger;
 const map = new EsriMap({
-  basemap: basemap
+  basemap: basemap,
+  ground: {
+    layers: [
+      new WCSLayer({
+        id: "custom-wcs-elevation-layer-id",
+        title: "custom-wcs-elevation-layer",
+        spatialReference: { wkid: 25833 }
+      })
+    ]
+  }
 });
 
 const view = new SceneView({
   map: map,
   container: "viewDiv",
+  viewingMode: "local",
   spatialReference: { wkid: 25833 },
   extent: {
     xmin: 213029.39990352467,
@@ -36,3 +57,55 @@ const view = new SceneView({
     spatialReference: { wkid: 25833 }
   }
 });
+
+const displayWidget = getWcsDisplayWidget();
+const imgNode = displayWidget.firstChild as HTMLImageElement;
+view.ui.add(displayWidget, "bottom-right");
+
+function getWcsDisplayWidget() {
+  const container = document.createElement("div");
+  container.className = "esri-widget";
+  container.style.cssText = "padding: 0.2rem; background: #fff";
+
+  const img = document.createElement("img");
+  img.src = "https://via.placeholder.com/256";
+
+  container.appendChild(img);
+
+  return container;
+}
+
+// watchUtils.whenTrue(view, "stationary", () => {
+//   const extent = view.extent;
+
+//   const { xmin, ymin, xmax, ymax } = extent;
+//   const heightToWidthRatio = extent.height / extent.width;
+
+//   const imageWidth = 256;
+//   const imageHeight = 256 * heightToWidthRatio;
+
+//   const url = `https://wms.geonorge.no/skwms1/wcs.dtm?SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&FORMAT=xyz&WIDTH=${Math.floor(
+//     imageWidth
+//   )}&HEIGHT=${Math.floor(
+//     imageHeight
+//   )}&COVERAGE=land_utm33_10m&crs=EPSG:25833&BBOX=${xmin},${ymin},${xmax},${ymax}`;
+
+//   WCSLayer(url)
+//     .then(image => {
+//       imgNode.src = image;
+//     })
+//     .catch(error => {
+//       console.warn("WCS ERROR");
+//     });
+// });
+
+// WCSLayer(
+//   "https://wms.geonorge.no/skwms1/wcs.dtm?SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&FORMAT=geotiff&WIDTH=256&HEIGHT=256&COVERAGE=land_utm33_10m&crs=EPSG:25833&BBOX=213029,6638602,309501,6735074"
+// )
+//   .then(image => {
+//     debugger;
+//     imgNode.src = image;
+//   })
+//   .catch(err => {
+//     debugger;
+//   });
